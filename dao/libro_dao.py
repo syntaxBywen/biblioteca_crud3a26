f348c379edb412d5e1ea6086ab223136ee867e45
joseangelcuevas07 <joseangelcuevas55@gmail.com>
2026-06-16 10:28:38 -0600

    Funciones completas de LibroDAO

--- dao/libro_dao.py
+++ dao/libro_dao.py
@@ -10,7 +10,7 @@ class libroDAO:
     def obtener_todos(self):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
-        
+
         cursor.execute("SELECT * FROM libro")
         registros = cursor.fetchall()
         
@@ -18,7 +18,61 @@ class libroDAO:
         for registro in registro:
             libro = Libro(registro.id, registro.titulo, registro.autor, registro.isbn,registro.disponible)
             libros.append(libro)
-            
         cursor.close()
         conexion.close()
         return libros
+    
+    def insertar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        sql = """
+        INSERT INTO libro (titulo, autor, isbn, disponible)
+        VALUES (%s, %s, %s, %s)
+        """
+        
+        cursor.execute(
+            sql,
+            (libro.titulo,
+             libro.autor, 
+             libro.isbn,
+             libro.disponible)
+        )
+        
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+        
+        
+    def actualizar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        sql = """
+        UPDATE libro 
+        SET titulo = %s, autor = %s, isbn = %s,
+        disponible = %s
+        WHERE id = %s
+        """
+        cursor.execute(
+            (libro.titulo,
+             libro.autor, 
+             libro.isbn,
+             libro.disponible,
+             libro.id)
+        )
+        
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+        
+    def eliminar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        
+        cursor.execute(
+            "DELETE FROM libro WHERE id = %s",
+            (libro_id,)
+          )
+        
+        conexion.commit()
+        cursor.close()
+        conexion.close()
