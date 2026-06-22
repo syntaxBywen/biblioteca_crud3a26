aeaac6612f7d79e63510226dacba15028b8e38c4
syntaxBywen <wenfdez26@gmail.com>
2026-06-22 10:34:36 -0600

    Mejorando la consulta de libros

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dao.libro_dao import LibroDAO
 from models.libro import Libro
 
-def main():
+def ver_libros():
     try:
         libro_dao = LibroDAO()
         
@@ -23,6 +23,44 @@ def main():
     except Exception as e:
         print("Error: ")
         print(e)
+
+def insertar_libro():
+    titulo = input("Escribe el titulo del nuevo libro: ")
+    autor = int(input("Escribe el id del autor"))
+    isbn = input("Escribe el isbn del nuevo libro: ")
+    disponible = True
+    try:
+        libro_dao = LibroDAO()
+        id = libro_dao.obtener_ultimo_id() + 1
+        print(f"id: {id}")
+        libro = Libro(id, titulo, autor, isbn, disponible)
+        libro_dao.insertar(libro)
+        print("Insercion realizada correctamente.")
+    except Exception as e:
+        print("Error al insertar un nuevo libro: ")
+        print(e)
+
+
+def main():
+    print("=== BIBLIOTECA UNIVERSITARIA ===")
+    print("Menu de opciones")
+    print("1. Ver todos los libros")
+    print("2. Insertar un nuevo libro")
+    print("3. Actualizar un libro")
+    print("4. Eliminar un libro")
+    opcion = int(input("Seleciona una opcion (1-4): "))
+
+    match opcion:
+        case 1:
+            ver_libros()
+        case 2:
+            insertar_libro()
+        case 3:
+            actualizar_libro()
+        case 4:
+            eliminar_libro()       
+
         
 if __name__ == "__main__":
     main()        
+
